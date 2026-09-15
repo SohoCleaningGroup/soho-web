3cf8a89504902e387ad83299ea2ee70f69feea30
Laxman Singh <laxman@novvr.com>
2026-09-16 00:42:20 +0530

    fix: og image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     url: "/",
     images: [
       {
-        url: "/opengraph-image.png",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "SoHo Cleaning Group",
